919476dd6c6f3239614d7d83e4dd54767088a921
furqankhalid729 <furqank729@gmail.com>
2020-08-04 12:35:29 +0500

    new changes

--- BookStore_project/settings.py
+++ BookStore_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import ENVIRONMENT as ENVIRONMENT
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -173,3 +175,14 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+if ENVIRONMENT == 'production':
+        SECURE_BROWSER_XSS_FILTER = True
+        X_FRAME_OPTIONS = 'DENY'
+        SECURE_SSL_REDIRECT = True
+        SECURE_HSTS_SECONDS = 3600
+        SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+        SECURE_HSTS_PRELOAD = True
+        SECURE_CONTENT_TYPE_NOSNIFF = True
+        SESSION_COOKIE_SECURE = True
+        CSRF_COOKIE_SECURE = True
+        SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
